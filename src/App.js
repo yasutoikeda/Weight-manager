7ad977507ab415756620eb796e00e65a9fe3bdb2
yasutoikeda <ikeda.14.8310@icloud.com>
2020-02-13 21:26:21 +0900

    TOP画面にタイトル表示

--- src/App.js
+++ src/App.js
@@ -6,21 +6,32 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
+          Hello World!!!
+        </p> */}
+
+        <h1>
+          Weight-manager
+        </h1>
+
+        
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+         
+
       </header>
     </div>
   );
 }
 
 export default App;
+
+
